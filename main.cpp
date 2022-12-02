880b97f05abb9bd90eae996b9fd4d8bb1bfd50b6
serega2 <serg.cheradionov@mail.ru>
2022-12-02 23:08:32 +0700

    2

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <clocale>
 using namespace std;
 double my_pow(double a, unsigned int b);
-
+double BinaryPower(double b, unsigned long long e);
+double FastPowerDividing(double b, double e);
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -14,6 +15,8 @@ int main()
     cout << "Введи степень, в которую будете возводить число " << endl;
     cin >> b;
     my_pow(a, b);
+    cout << BinaryPower(a, b) << endl;
+    cout << FastPowerDividing(a, b) << endl;
 }
 
 double my_pow(double a, unsigned int b)
@@ -27,6 +30,40 @@ double my_pow(double a, unsigned int b)
     return 1;
 }
 
+double OldApproximatePower(double b, double e) {
+    union {
+        double d;
+        long long i;
+    } u = { b };
+    u.i = (long long)(4606853616395542500L + e * (u.i - 4606853616395542500L));
+    return u.d;
+}
+double BinaryPower(double b, unsigned long long e) {
+    double v = 1.0;
+    while (e != 0) {
+        if ((e & 1) != 0) {
+            v *= b;
+        }
+        b *= b;
+        e >>= 1;
+    }
+    return v;
+}
+double FastPowerDividing(double b, double e) {
+    if (b == 1.0 || e == 0.0) {
+        return 1.0;
+    }
+
+    double eAbs = fabs(e);
+    double el = ceil(eAbs);
+    double basePart = OldApproximatePower(b, eAbs / el);
+    double result = BinaryPower(basePart, (unsigned long long)el);
+
+    if (e < 0.0) {
+        return 1.0 / result;
+    }
+    return result;
+}
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
